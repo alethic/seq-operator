50c791080d8697456bd05ffa9d2f128dd556d65c
Jerome Haltom <jhaltom@alethic.solutions>
2025-11-10 09:41:46 -0600

    Add RetentionPolicy.

---
+++ src/Alethic.Seq.Operator.Core/Models/RetentionPolicy/RetentionPolicyConf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Alethic.Seq.Operator.Core.Models.RetentionPolicy
+{
+
+    public class RetentionPolicyConf
+    {
+
+        [JsonPropertyName("retentionTime")]
+        public string? RetentionTime { get; set; }
+
+    }
+
+}
